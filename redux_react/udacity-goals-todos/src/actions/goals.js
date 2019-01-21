2611383abb3b2b1acc35811e615fbb49d3004357
Matt Coleman <coleman.matt319@gmail.com>
2019-01-20 20:36:40 -0500

    Created React app, organized code into Rails-style folder structure

---
+++ redux_react/udacity-goals-todos/src/actions/goals.js
@@ -0,0 +1,41 @@
+import API from 'goals-todos-api'
+export const ADD_GOAL = 'ADD_GOAL'
+export const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addGoal (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoal (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+export function handleAddGoal(goal, callback) {
+  return (dispatch) => {
+    return API.saveGoal(goal)
+        .then((goal) => {
+          dispatch(addGoal(goal))
+          callback()
+        })
+        .catch(() => {
+          alert('An error occurred. Please try again.')
+        })
+  }
+}
+
+export function handleDeleteGoal(goal) {
+  return (dispatch) => {
+    dispatch(removeGoal(goal.id))
+    return API.deleteGoal(goal.id)
+        .catch(() => {
+          dispatch(addGoal(goal))
+          alert('An error occurred. Please try again.')
+        })
+  }
+}
